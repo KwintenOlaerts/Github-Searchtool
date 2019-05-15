580335a87023b1304b221cbf9ee94d3990c49ab8
Kwinten Olaerts <olaertskwinten@gmail.com>
2019-05-15 14:13:48 +0200

    new features

--- ui.js
+++ ui.js
@@ -2,14 +2,14 @@ class UI {
     constructor() {
         this.profile = document.getElementById('profile');        
     }
-
+// display profile in ui
     showProfile(user) {
         this.profile.innerHTML= `
      <div class="card card-body mb-3">
          <div class="row">
              <div class="col md-3">
              <img class="img-fluid mb-2" src="${user.avatar_url}">
-             <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block">
+             <a href="${user.html_url}" target="_blank" class="btn btn-primary btn-block mb-4">
             View Profile</a>
              </div>
              <div class="col md-9">
@@ -23,7 +23,7 @@ class UI {
                  <li class="list-group-item>Website/Blog: ${user.blog}</li>
                  <li class="list-group-item>Location: ${user.location}</li>
                  <li class="list-group-item>Member since: ${user.created_at}</li>
-                 <li class="list-group-item>Last deployment: ${user.updated_at}</li>
+                 <li class="list-group-item>Name: ${user.name}</li>
                  </ul>
 
              </div>
@@ -32,5 +32,37 @@ class UI {
      <h3 class="page-heading mb-3>Latest Repos</h3>
      <div id="repos"></div>
      `;
+}
+// show alert message
+showAlert(message, className){
+    // Clear any remaining alerts
+    this.clearAlert();
+    //create div
+    const div = document.createElement('div');
+    // add classas
+    div.className = className;
+    // add text
+    div.appendChild(document.createTextNode(message));
+    // get parent
+    const container = document.querySelector('.searchContainer')
+    // Get search box
+    const search = document.querySelector('.search');
+    // Insert alert
+    container.insertBefore(div, search);
+    //Timeout after 3 seconds
+    setTimeout(() => {
+        this.clearAlert();
+    }, 3000);
+}
+// Clear alert message
+clearAlert() {
+    const currentAlert = document.querySelector('.alert');
+    if(currentAlert){
+        currentAlert.remove();
+    }
+}
+//clear profile
+clearProfile() {
+    this.profile.innerHTML = '';
 }
 }
